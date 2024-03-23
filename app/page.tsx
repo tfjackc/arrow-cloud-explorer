f68ef4b2ca458eebc210b16141f629a67b5a0a55
tfjackc <pnwest.geo@gmail.com>
2024-03-22 22:57:21 -0700

    server side file upload

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,24 @@
+import {Button} from "@nextui-org/react";
 export default function Home() {
-  return (
-    <main className="">
-      <div className="text-3xl">
-        Landing Page
+
+    async function create(formData: FormData) {
+        'use server';
+        const file = formData.get('upload_file') as File;
+        console.log('file', file);
+    }
+
+    return (
+    <main>
+      <div className="flex flex-col items-center justify-center h-screen px-4 m-2">
+          <form action={create}>
+              <p className="mb-6">
+                  <input type="file"
+                         name='upload_file'
+                         required
+                  />
+              </p>
+              <Button color="primary" type="submit">Submit</Button>
+          </form>
       </div>
     </main>
   );
